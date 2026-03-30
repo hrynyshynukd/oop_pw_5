53119179af0ecc464dcb5b8971d857e3292439fd
hrynyshynukd <viktoriia.r.hrynyshyn@ukd.edu.ua>
2026-03-30 21:40:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,55 +3,57 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        // Автори
         Author author1 = new Author("Тарас Шевченко", 1814);
         Author author2 = new Author("Іван Франко", 1856);
 
-        // Книги
         Book book1 = new Book("Кобзар", author1, 1840, "Збірка поезій");
         Book book2 = new Book("Захар Беркут", author2, 1883, "Історична повість");
         Book book3 = new Book("Іван Підкова", author1, 1840, "");
 
-        // Бібліотека
         Library library = new Library("Моя бібліотека");
 
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
-
-        // Дублікат (перевірка)
         library.addBook(book1);
 
-        // Вивід
-        System.out.println("=== Усі книги ===");
-        library.listBooks();
-
-        // Пошук по автору
-        System.out.println("\n=== Книги Шевченка ===");
-        List<Book> found = library.findBooksByAuthor("Тарас Шевченко");
-        for (Book b : found) {
-            System.out.println(b.getInfo());
+        System.out.println("\n=== УСІ КНИГИ ===");
+        for (String info : library.listBooks()) {
+            System.out.println(info);
+            System.out.println("------------------");
         }
 
-        // Пошук по назві
-        System.out.println("\n=== Пошук 'Іван' ===");
-        for (Book b : library.searchByTitle("Іван")) {
+        System.out.println("\n=== КНИГИ ШЕВЧЕНКА ===");
+        List<Book> byAuthor = library.findBooksByAuthor("Тарас Шевченко");
+        for (Book b : byAuthor) {
             System.out.println(b.getInfo());
+            System.out.println("------------------");
         }
 
-        // Видалення
+        System.out.println("\n=== ПОШУК: 'Іван' ===");
+        List<Book> search = library.searchByTitle("Іван");
+        for (Book b : search) {
+            System.out.println(b.getInfo());
+            System.out.println("------------------");
+        }
+
         library.removeBook("Кобзар");
 
-        System.out.println("\n=== Після видалення ===");
-        library.listBooks();
+        System.out.println("\n=== ПІСЛЯ ВИДАЛЕННЯ ===");
+        for (String info : library.listBooks()) {
+            System.out.println(info);
+            System.out.println("------------------");
+        }
 
-        // Читач
-        Reader reader = new Reader("Олена");
+        Reader reader = new Reader("Степан");
+        library.addReader(reader);
 
-        reader.borrowBook(book2);
+        System.out.println("\n=== ВЗЯТИ КНИГУ ===");
+        library.lendBook(reader, book2);
         reader.showBorrowedBooks();
 
-        reader.returnBook(book2);
+        System.out.println("\n=== ПОВЕРНУТИ КНИГУ ===");
+        library.returnBook(reader, book2);
         reader.showBorrowedBooks();
     }
 }
